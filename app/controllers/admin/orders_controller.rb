9f548f1c800b28786e1613f663db7827303168fc
myPERislow <satotaku.kamigou@gmail.com>
2018-02-20 16:11:17 +0900

    admin controllerにログインしていなければアクセスできない機能を追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,6 @@
 class Admin::OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :show, :edit, :update, :destory]
 
   def index
     @orders = Order.all
