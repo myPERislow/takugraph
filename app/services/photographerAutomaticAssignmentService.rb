44b1c5e35bfccb949f57260e3b846aebe4098769
myPERislow <satotaku.kamigou@gmail.com>
2018-03-09 11:57:00 +0900

    use_service_for_order_controller

--- app/services/photographerAutomaticAssignmentService.rb
+++ app/services/photographerAutomaticAssignmentService.rb
@@ -1,15 +1,45 @@
 class PhotographerAutomacitAssignmentService
-  THRESHOLD_OD_DIFFERENCE = 8
 
-  def initialize(photographers, order)
-    raise TypeError, "Order expected, get #order.class" unless order.is_a? Order
-    @photographers = photographers
+  def initialize(order)
     @order = order
+    @area = @order.area
+    photographers = User.all.photographer.where(area_id: @area.id)
+
+      # raise InvalidPhotographerAssignment unless PhotographerAutomacitAssignmentService.new(photographers, @order).execute
+
+      # 第二段階(計二段階中) - dateによる絞り込み
+
+    @photographers = photographers
     @date = @order.day
 　end
 
   def execute
-    return false unless @date.present?
-    photographer = @photographers.select { |photographer| photographer.schedules.where(target_day: @date).priority == true}.sample
+    # シンプルに回す
+    photographer_schedules = @photographers.map { |e| e.schedules  }
+
+    l = []
+    photographer_schedules.each do |photographer_schedule|
+      photographer_schedule.each do |photographer|
+        if (photographer.target_day == @date && photographer.priority == true)
+          l.push(photographer)
+        end
+      end
+    end
+    if l
+      t = l.sample
+    else
+      @order.save
+      OrderMailer.post_order().deliver_later
+      redirect_to root_path
+    end
+    if t.nil?
+      @order.save
+      OrderMailer.post_order().deliver_later
+      redirect_to root_path
+    else
+      @order.update(photographer_id: t.id)
+      OrderMailer.photographer_post_order().deliver_later
+      redirect_to root_path
+    end
   end
 end
