8a8946090cede5b8bbc3470a375cc3e6cd85e44c
myPERislow <satotaku.kamigou@gmail.com>
2018-03-15 16:14:02 +0900

    orderのvalidationも追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,7 @@
 class Order < ApplicationRecord
   # first_name属性に値が存在しない場合バリデーションエラーになります
-  validates :first_name, presence: true
-  # last_name属性に値が存在しない場合バリデーションエラーになります
-  validates :last_name, presence: true
-  # day属性に値が存在しない場合バリデーションエラーになります
-  validates :day, presence: true
-  # location属性に値が存在しない場合バリデーションエラーになります
-  validates :location, presence: true
-  # phone_number属性に値が存在しない場合バリデーションエラーになります
-  validates :phone_number, presence: true
+  validates :email, presence: true
+
   # Railsの慣例では、相手のモデル上の外部キーを保持しているカラム名については、そのモデル名にサフィックス_idを追加した関連付け名が使用されることを前提としている。
   # :foreign_keyオプションを使用すると外部キーの名前を直接してすることができる。
   has_one :album, dependent: :destroy, inverse_of: :order
