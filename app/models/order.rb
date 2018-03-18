1649abd51156bae1d03448009aa2129a2430700f
myPERislow <satotaku.kamigou@gmail.com>
2018-03-18 13:55:39 +0900

    orderモデルのareaへのoptional: trueを削除

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   # Railsの慣例では、相手のモデル上の外部キーを保持しているカラム名については、そのモデル名にサフィックス_idを追加した関連付け名が使用されることを前提としている。
   # :foreign_keyオプションを使用すると外部キーの名前を直接してすることができる。
   has_one :album, dependent: :destroy, inverse_of: :order
-  belongs_to :area, optional: true
+  belongs_to :area
 
   has_one :addtional_plans
   accepts_nested_attributes_for :addtional_plans
